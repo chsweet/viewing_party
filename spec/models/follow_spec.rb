f4b24bb274b3a8146f37262b8cedfe0f2680fe3d
Carina Sweet <carina.sweet06@gmail.com>
2021-08-24 13:16:21 -0600

    Update follow model and tests

--- spec/models/follow_spec.rb
+++ spec/models/follow_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe Follow do
   describe 'relationships' do
-    it {should belong_to :hosts}
-    it {should belong_to :followeds}
+    it {should belong_to :host}
+    it {should belong_to :followed}
   end
 end
