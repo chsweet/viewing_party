7b4d76e6ca86e87e5b07bc57675d8dc979ee2b98
Carina Sweet <carina.sweet06@gmail.com>
2021-08-24 13:16:44 -0600

    Update user model and tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  has_many :host_follows, foreign_key: :user_id, class_name: 'Follow'
+  has_many :host_follows, foreign_key: :host_id, class_name: 'Follow'
   has_many :hosts, through: :host_follows
 
   has_many :followed_follows, foreign_key: :followed_id, class_name: 'Follow'
