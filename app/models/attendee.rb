7add47027b2c3195b9fc369cb74a9173865df76b
Marla Schulz <marlaa.schulz@gmail.com>
2021-08-24 13:23:44 -0700

    Add validations

--- app/models/attendee.rb
+++ app/models/attendee.rb
@@ -1,4 +1,7 @@
 class Attendee < ApplicationRecord
   belongs_to :party
   belongs_to :user
+
+  validates :party_id, presence: true
+  validates :user_id, presence: true
 end
--- spec/models/attendee_spec.rb
+++ spec/models/attendee_spec.rb
@@ -5,4 +5,9 @@ RSpec.describe Attendee do
     it {should belong_to :party}
     it {should belong_to :user}
   end
+
+  describe 'validations' do
+    it {should validate_presence_of :party_id}
+    it {should validate_presence_of :user_id}
+  end
 end
