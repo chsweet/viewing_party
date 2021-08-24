a0698119dafe278f0f74c13d10b795be591c6e40
Carina Sweet <carina.sweet06@gmail.com>
2021-08-24 15:32:44 -0600

    Update rubocop errors

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,12 @@
 class User < ApplicationRecord
-  has_many :host_follows, foreign_key: :host_id, class_name: 'Follow'
+  has_many :host_follows, foreign_key: :host_id, class_name: 'Follow', dependent: :destroy
   has_many :hosts, through: :host_follows
 
-  has_many :followed_follows, foreign_key: :followed_id, class_name: 'Follow'
+  has_many :followed_follows, foreign_key: :followed_id, class_name: 'Follow', dependent: :destroy
   has_many :followeds, through: :followed_follows
 
-  has_many :parties
-  has_many :attendees
+  has_many :parties, dependent: :destroy
+  has_many :attendees, dependent: :destroy
 
   validates :email, presence: true, uniqueness: true
 
--- spec/factories/party.rb
+++ spec/factories/party.rb
@@ -3,7 +3,6 @@ FactoryBot.define do
     movie { Faker::Internet.email }
     association :user, factory: :user
     duration { Faker::Number.between(from: 90, to: 200)}
-    date { Faker::Date.forward(days:23) }
-    # start_time { Faker::Time.}
+    date_time { Faker::Time.forward(days: 23, period: :afternoon) }
   end
 end
