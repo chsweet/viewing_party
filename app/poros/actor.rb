6855a8ca7b40ab2cd8c43830f9e5933d55158f0f
Marla Schulz <marlaa.schulz@gmail.com>
2021-08-26 14:42:13 -0700

    Create movie db service and poros

---
+++ app/poros/actor.rb
@@ -0,0 +1,9 @@
+class Actor
+  attr_reader :name
+              :role
+  def initialize (data)
+    @name = data[:name]
+    @role = data[:character]
+    @department = data[:known_for_department]
+  end
+end
