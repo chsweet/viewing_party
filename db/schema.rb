b581dc3d2562383d88d6e05e977b62161da0e6e5
Carina Sweet <carina.sweet06@gmail.com>
2021-08-26 16:32:11 -0600

    Add movie_id to parties table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_23_233102) do
+ActiveRecord::Schema.define(version: 2021_08_26_222819) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -38,6 +38,7 @@ ActiveRecord::Schema.define(version: 2021_08_23_233102) do
     t.datetime "date_time"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "movie_id"
     t.index ["user_id"], name: "index_parties_on_user_id"
   end
 
