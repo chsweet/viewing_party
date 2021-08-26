72a9da19e5db65a838c4b7000782c717b00d2254
Marla Schulz <marlaa.schulz@gmail.com>
2021-08-25 19:30:56 -0700

    Add authentication requirements

--- spec/features/movies/discover_spec.rb
+++ spec/features/movies/discover_spec.rb
@@ -1,14 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe 'Discover' do
-  before(:each) do
-    @user1 = create(:user)
-
-    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@user1)
-    
-    visit('/discover')
-  end
   describe 'authenticated user' do
+    before(:each) do
+      @user1 = create(:user)
+
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@user1)
+
+      visit('/discover')
+    end
+
     it 'can find top rated movies' do
       expect(page).to have_content("Welcome #{@user1.email}")
       click_on('Find Top Rated Movies')
@@ -17,12 +18,15 @@ RSpec.describe 'Discover' do
     end
 
     it 'can search by movie title' do
+      fill_in(:search, with:('Titanic'))
+      click_on('Find Movies')
 
+      expect(current_path).to eq(movies_path)
     end
   end
 
   describe 'unauthenticated user' do
-    xit 'redirects to root path' do
+    it 'redirects to root path' do
       expect(page).to have_content("Uh Oh, Can't go there! Try logging in or registering as a new user.")
       expect(current_path).to eq(root_path)
     end
