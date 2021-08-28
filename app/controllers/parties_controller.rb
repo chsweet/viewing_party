ec422c58b26357d0891b032963092bfad0773fe2
Carina Sweet <carina.sweet06@gmail.com>
2021-08-28 17:07:11 -0600

    Update form time

--- app/controllers/parties_controller.rb
+++ app/controllers/parties_controller.rb
@@ -8,13 +8,15 @@ class PartiesController < ApplicationController
   def create
     json_movie = MovieDbService.new.movie_details(params[:party][:movie_id])
     @movie = Movie.new(json_movie)
-    time = DateTime.parse(params[:party]['date_time(1i)']+params[:party]['date_time(2i)']+params[:party]['date_time(3i)']+'T'+params[:party]['date_time(4i)']+params[:party]['date_time(5i)'])
+    time = Time.new(params[:party]['date_time(1i)'].to_i, params[:party]['date_time(2i)'].to_i, params[:party]['date_time(3i)'].to_i, params[:party]['date_time(4i)'].to_i, params[:party]['date_time(5i)'])
     party = Party.create!(movie_id: @movie.id, movie: @movie.title, user_id: current_user.id, duration: params[:party][:duration], date_time: time)
 
-    params[:party][:invited].each do |key, value|
-       if value == '1'
-         Attendee.create!(party_id: party.id, user_id: key.to_i)
-       end
+    unless current_user.user_friends.empty?
+      params[:party][:invited].each do |key, value|
+         if value == '1'
+           Attendee.create!(party_id: party.id, user_id: key.to_i)
+         end
+      end
     end
 
     redirect_to dashboard_path
