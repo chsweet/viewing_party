60401f09f35b8f61b91ec97a0fb4f377e0419469
Marla Schulz <marlaa.schulz@gmail.com>
2021-08-27 17:31:56 -0700

    Add movie details

--- spec/features/movies/show_spec.rb
+++ spec/features/movies/show_spec.rb
@@ -7,12 +7,13 @@ RSpec.describe 'movies show page' do
     allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@user_1)
   end
 
-  it 'can show movie title and summary', :vcr do
-    # VCR.use_cassette('movie_details') do
-      visit movie_path(550)
+  it 'can show movie title, summary, vote average, runtime, genre', :vcr do
+    visit movie_path(550)
 
-      expect(page).to have_content("Fight Club")
-      expect(page).to have_content("A ticking-time-bomb insomniac and a slippery soap salesman channel primal male aggression into a shocking new form of therapy. Their concept catches on, with underground \"fight clubs\" forming in every town, until an eccentric gets in the way and ignites an out-of-control spiral toward oblivion.")
-    # end
+    expect(page).to have_content("Fight Club")
+    expect(page).to have_content('Vote Average: 8.4')
+    #expect(page).to have_content('Runtime: 2 hr 19 min')
+    #expect(page).to have_content('Genre(s): Drama')
+    expect(page).to have_content("A ticking-time-bomb insomniac and a slippery soap salesman channel primal male aggression into a shocking new form of therapy. Their concept catches on, with underground \"fight clubs\" forming in every town, until an eccentric gets in the way and ignites an out-of-control spiral toward oblivion.")
   end
 end
