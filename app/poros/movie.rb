2b39a36a61435c9d1626201b5b30f3d4c1b498f8
Marla Schulz <marlaa.schulz@gmail.com>
2021-08-27 17:32:20 -0700

    Add genre names and hours methods

--- app/poros/movie.rb
+++ app/poros/movie.rb
@@ -11,6 +11,7 @@ class Movie
     @id = data[:id]
     @title = data[:original_title]
     @vote_average = data[:vote_average]
+    #convert to hours and minutes
     @runtime = data[:runtime]
     @genres = genre_names(data[:genres])
     @summary = data[:overview]
@@ -19,7 +20,17 @@ class Movie
 
   end
 
-  def genre_names(array)
+  def genre_names(data_array)
+    data_array.map do |data|
+      data[:name]
+    end
+  end
+
+  def hours
+    @runtime/60
+  end
+
+  def minutes
 
   end
 end
--- spec/poros/movie_spec.rb
+++ spec/poros/movie_spec.rb
@@ -2,7 +2,8 @@ require 'rails_helper'
 
 RSpec.describe Movie do
   before(:each) do
-    @data = {original_title: 'Aladdin', vote_average: '7.8', runtime: '245', genres: ['comedy', 'animation'], overview: 'A genie comes out of a bottle', id: 4}
+    @data = {original_title: 'Aladdin', vote_average: 7.8, runtime: 139, genres: [{id: 18, name: 'comedy'}, {id: 5, name: 'animation'}], overview: 'A genie comes out of a bottle', id: 4}
+    @data2 = {original_title: 'Aladdin', vote_average: 7.8, runtime: 175, genres: [{id: 18, name: 'comedy'}, {id: 5, name: 'animation'}], overview: 'A genie comes out of a bottle', id: 4}
   end
 
   describe 'attributes' do
@@ -12,6 +13,28 @@ RSpec.describe Movie do
   end
 
   describe 'instance methods' do
-    
+    before(:each) do
+      @movie = Movie.new(@data)
+      @movie2 = Movie.new(@data2)
+    end
+
+    describe 'genre names' do
+      it 'returns an array of movie genres' do
+        expect(@movie.genre_names(@data[:genres])).to eq(['comedy', 'animation'])
+      end
+    end
+
+    describe 'hours' do
+      it 'returns movie hours' do
+        expect(@movie.hours).to eq(2)
+        expect(@movie2.hours).to eq(2)
+      end
+    end
+
+    describe 'minutes' do
+      it 'returns movie minutes' do
+        expect(@movie.minutes).to eq(19)
+      end
+    end
   end
 end
