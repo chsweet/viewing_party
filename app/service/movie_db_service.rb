d574fef2706576fc57da7a902c7f5ed28e3a6f95
Carina Sweet <carina.sweet06@gmail.com>
2021-08-30 16:37:53 -0600

    remove redirect

--- app/service/movie_db_service.rb
+++ app/service/movie_db_service.rb
@@ -26,8 +26,8 @@ class MovieDbService
 
   def get_data(endpoint)
     conn = Faraday.new(url: "https://api.themoviedb.org/") do |faraday|
-      faraday.use FaradayMiddleware::FollowRedirects, limit: 5
-      faraday.adapter Faraday.default_adapter
+      # faraday.use FaradayMiddleware::FollowRedirects, limit: 5
+      # faraday.adapter Faraday.default_adapter
       faraday.params['api_key'] = ENV['movie_api_key']
     end
     response = conn.get(endpoint)
